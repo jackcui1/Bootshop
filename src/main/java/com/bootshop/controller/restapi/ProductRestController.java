8038d0aa0e679a35606b7a59893460d65cc2d8ec
Guowei <cuigw28@gmail.com>
2018-10-04 23:15:41 -0400

    Add restful API of product list.

--- src/main/java/com/bootshop/controller/restapi/ProductRestController.java
+++ src/main/java/com/bootshop/controller/restapi/ProductRestController.java
@@ -1,6 +1,9 @@
 package com.bootshop.controller.restapi;
 
+import com.bootshop.controller.ImagesController;
+import com.bootshop.model.Category;
 import com.bootshop.model.Product;
+import com.bootshop.service.CategoryService;
 import com.bootshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.util.List;
 
 /**
  * @author Guowei Cui
@@ -22,10 +28,28 @@ public class ProductRestController {
     @Autowired
     ProductService productService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     @RequestMapping("/{id}")
     public Product getProductById(@PathVariable("id") int id , Model model ) {
         Product product = productService.getProductById(id);
         return product;
     }
 
+    @RequestMapping("")
+    public List<Product> getProductList( Model model ) {
+        List<Product> products = productService.getProductList();
+
+        for(Product product:products){
+            String getFilename= MvcUriComponentsBuilder
+                    .fromMethodName(ImagesController.class,
+                            "getFile", product.getImagename()).build().toString();
+            product.setAbsolutImagename(getFilename);
+        }
+        System.out.println(products.get(0).getAbsolutImagename());
+
+        return products;
+
+    }
 }
